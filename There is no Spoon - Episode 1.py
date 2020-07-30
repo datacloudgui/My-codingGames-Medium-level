760d7f9622bb813b828af757090dd36bad7fa338
datacloudgui <guillermosanchezh@gmail.com>
2020-07-29 21:14:31 -0500

    finish to navigate and find nodes score 100

--- There is no Spoon - Episode 1.py
+++ There is no Spoon - Episode 1.py
@@ -19,8 +19,9 @@ for i in range (height):
             line.append(j)
             line.append(i)
             if j+1<len(h_lines[i]):
-                if h_lines[i][j+1]=='0':
-                    line.append(j+1)
+                h_line_0_validation = h_lines[i].find('0',j+1) #find the next 0 in the line
+                if h_line_0_validation != -1:
+                    line.append(h_line_0_validation)
                     line.append(i)
                 else:
                     line.append(-1)
@@ -29,9 +30,14 @@ for i in range (height):
                 line.append(-1)
                 line.append(-1)
             if i+1<height:
-                if h_lines[i+1][j]=='0':
+                height_0_index = -1 #To store the index if is a node in the present column
+                for k in range(i+1,height):
+                    if h_lines[k][j]=='0':
+                        height_0_index = k
+                        break
+                if height_0_index!=-1:
                     line.append(j)
-                    line.append(i+1)
+                    line.append(height_0_index)
                 else:
                     line.append(-1)
                     line.append(-1)
@@ -44,10 +50,3 @@ for i in range(len(output_line)):
     for j in range(len(output_line[i])):
         print(output_line[i][j],end=' ')
     print('')
-
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-
-# Three coordinates: a node, its right neighbor, its bottom neighbor
-#print("0 0 1 0 0 1")
